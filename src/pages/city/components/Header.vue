d7b1e8f59c62837703ac9d71bc8868804977ab3d
qinbinyuan <binyuan_qin@163.com>
2019-06-20 20:57:00 +0800

    城市路由

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+    <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/variable.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    coloor: #fff
+    background: $bgColor
+    font-style: .4rem
+    .header-back
+      position: absolute
+      top:0
+      left 0
+      width: .64rem
+      text-align:center
+      font-style: .4rem
+      color: #ffffff
+</style>
