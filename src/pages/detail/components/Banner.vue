c7d7e5945f98f9ae237ee40969f5a795996041a2
qinbinyuan <binyuan_qin@163.com>
2019-06-21 16:47:59 +0800

    图片详情

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+        class="banner-img"
+        src="http://img1.qunarzz.com/sight/p0/1902/7a/7ac34ad6cc6e7497a3.img.jpg_600x330_db067039.jpg">
+      <div class="banner-info">
+        <div class="banner-title">东莞龙凤山庄影视渡假村(AAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary
+      @close="handleGallaryClose"
+      v-show="showGallary" :imgs="imgs"></common-gallary>
+  </div>
+</template>
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1905/af/af4f95f579ee43b1a3.water.jpg_600x330_164b10ec.jpg', 'http://img1.qunarzz.com/sight/p0/1511/34/34170b200db08a5a90.img.jpg_600x330_9fee26e9.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #ffffff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .4rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .32rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
