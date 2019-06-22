2d9f6661fed06ec01d2dbd0bc775f26846f57380
qinbinyuan <binyuan_qin@163.com>
2019-06-22 13:02:13 +0800

    详情页列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,44 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三管联票',
+          children: [{
+            title: '成人三管联票某一连锁店销售'
+          }]
+        }, {
+          title: '成人五管联票'
+        }, {
+          title: '成人七管联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
